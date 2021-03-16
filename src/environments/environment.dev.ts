da99c18a6d59e83b5102fee24cc669931def1d22
insyirahshauki <sitiinsyirah161296@gmail.com>
2021-03-16 17:09:17 +0800

    test

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,18 @@
+export const environment = {
+    production: false,
+    name: 'dev',
+    firebase: {
+        config: {
+            apiKey: "AIzaSyBNNdCE-N6ltbNyrL6Z0QLLSswCYp9JGIo",
+            authDomain: "courseapp-3aefe.firebaseapp.com",
+            projectId: "courseapp-3aefe",
+            storageBucket: "courseapp-3aefe.appspot.com",
+            messagingSenderId: "102464107908",
+            appId: "1:102464107908:web:88e58356eb56d8441419d1"
+        },
+        actionCodeSettings: {
+            url: 'http://localhost:5200/profile/new',
+            handleCodeInApp: true
+        }
+    }
+};
